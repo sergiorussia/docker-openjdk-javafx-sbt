115f843032ae2b26d5ee39f746023be3acfdaf20
sergiorussia <22471371+sergiorussia@users.noreply.github.com>
2019-09-25 00:07:14 +0300

    sbt 1.3.2, scala 2.13.1

---
+++ update.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Usage: $0 <sbt-version> <scala-version>
+
+# https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+set -Eeuo pipefail
+
+version_sbt="$1"
+version_scala="$2"
+root="$( cd "$( dirname "$0" )" && pwd )"
+cd "$root/"
+sed -i -e "s|^ENV SBT_VERSION.*|ENV SBT_VERSION $version_sbt|" -e "s|^ENV SCALA_VERSION.*|ENV SCALA_VERSION $version_scala|" image-*/Dockerfile
